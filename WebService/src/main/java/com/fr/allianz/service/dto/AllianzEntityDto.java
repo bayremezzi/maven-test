e0fee8afb202c01896fbb4fecdf6a809fc67d578
bayremezzi <bayram.ezzi@esprit.tn>
2022-02-06 23:57:33 +0100

    test build

--- WebService/src/main/java/com/fr/allianz/service/dto/AllianzEntityDto.java
+++ WebService/src/main/java/com/fr/allianz/service/dto/AllianzEntityDto.java
@@ -1,5 +1,7 @@
 package com.fr.allianz.service.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,8 +19,9 @@ import java.util.Date;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
+@ApiModel("Entity bean")
 public class AllianzEntityDto implements Serializable {
-
+    @ApiModelProperty(value = "Id of entity", example = "123456")
     private long id;
     private String label;
     private Date date;
--- model/src/main/java/com/fr/allianz/model/jsonParser/FileParser.java
+++ model/src/main/java/com/fr/allianz/model/jsonParser/FileParser.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fr.allianz.model.entities.AllianzEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -18,8 +19,8 @@ public class FileParser {
 
     private static final Logger logger = LoggerFactory.getLogger(FileParser.class.getName());
 
-    //@Value("/data.json")
-    //private String filePath;
+    @Value("${file-path}")
+    private String filePath;
 
     private List<AllianzEntity> allianzEntities = new ArrayList<>();
 
@@ -28,7 +29,7 @@ public class FileParser {
 
         try {
             allianzEntities = mapper
-                    .readValue(AllianzEntity.class.getResourceAsStream("/data.json"),
+                    .readValue(AllianzEntity.class.getResourceAsStream(filePath),
                             new TypeReference<>() {
                             });
         } catch (IOException e) {
